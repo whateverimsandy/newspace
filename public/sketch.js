21f0860fb70d71e72f8a9d6c029f65d32b2c1f5c
SandyVu <sandyvu1311@gmail.com>
2023-04-28 17:23:46 +1000

    description of changes

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,17 @@
 document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
-function setup () {
-   createCanvas (innerWidth, innerHeight)
-}
+const cnv = document.getElementById ("cnv_id")
+cnv.width = innerWidth
+cnv.height= innerHeight
+cnv.style.background = `turquoise`
 
-function  draw () {
-   background (`turquoise`)
+const ctx = cnv.getContext (`2d`)
+
+cnv.onpointerdown = () => {
+   //console.log (`interaction achieved`)
+   //console.dir(e)
+   console.log(e.x, e.y)
+   ctx.fillRect (e.x, e.y, 100, 100)
 }
+//console.dir (cnv)
